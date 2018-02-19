118a571914b23a70a5a1962f27f428bb3747d1db
Pavel Surmenok <surmenok@gmail.com>
2018-02-19 10:01:24 -0800

    Move the inference code into a class in test_yolo.py

--- test_yolo.py
+++ test_yolo.py
@@ -6,12 +6,14 @@ import imghdr
 import os
 import random
 
+from collections import namedtuple
+
 import numpy as np
 from keras import backend as K
 from keras.models import load_model
 from PIL import Image, ImageDraw, ImageFont
 
-from yad2k.models.keras_yolo import yolo_eval, yolo_head
+from yad2k.models.keras_yolo import yolo_eval, yolo_head, yolo_loss
 
 parser = argparse.ArgumentParser(
     description='Run a YOLO_v2 style detection model on test images..')
@@ -53,47 +55,115 @@ parser.add_argument(
     default=.5)
 
 
-def _main(args):
-    model_path = os.path.expanduser(args.model_path)
-    assert model_path.endswith('.h5'), 'Keras model must be a .h5 file.'
-    anchors_path = os.path.expanduser(args.anchors_path)
-    classes_path = os.path.expanduser(args.classes_path)
-    test_path = os.path.expanduser(args.test_path)
-    output_path = os.path.expanduser(args.output_path)
+Object = namedtuple('Object', ['class_name', 'top', 'left', 'bottom', 'right', 'score'])
 
-    if not os.path.exists(output_path):
-        print('Creating output path {}'.format(output_path))
-        os.mkdir(output_path)
+class YoloModel:
+    # class_names
+    # yolo_model
+    # model_image_size
+    # is_fixed_size
+    # input_image_shape
+    # boxes
+    # scores
+    # classes
 
-    sess = K.get_session()  # TODO: Remove dependence on Tensorflow session.
+    def __init__(self, model_path, anchors_path, classes_path, score_threshold, iou_threshold):
+        self.class_names = self._load_class_names(classes_path)
+        anchors = self._load_anchors(anchors_path)
+        self.yolo_model = load_model(model_path)
 
-    with open(classes_path) as f:
-        class_names = f.readlines()
-    class_names = [c.strip() for c in class_names]
+        # Verify model, anchors, and classes are compatible
+        num_classes = len(self.class_names)
+        num_anchors = len(anchors)
 
-    with open(anchors_path) as f:
-        anchors = f.readline()
-        anchors = [float(x) for x in anchors.split(',')]
-        anchors = np.array(anchors).reshape(-1, 2)
+        # TODO: Assumes dim ordering is channel last
+        model_output_channels = self.yolo_model.layers[-1].output_shape[-1]
+        assert model_output_channels == num_anchors * (num_classes + 5), \
+            'Mismatch between model and given anchor and class sizes. ' \
+            'Specify matching anchors and classes with --anchors_path and ' \
+            '--classes_path flags.'
 
-    yolo_model = load_model(model_path)
+        # Check if model is fully convolutional, assuming channel last order.
+        self.model_image_size = self.yolo_model.layers[0].input_shape[1:3]
+        self.is_fixed_size = self.model_image_size != (None, None)
 
-    # Verify model, anchors, and classes are compatible
-    num_classes = len(class_names)
-    num_anchors = len(anchors)
-    # TODO: Assumes dim ordering is channel last
-    model_output_channels = yolo_model.layers[-1].output_shape[-1]
-    assert model_output_channels == num_anchors * (num_classes + 5), \
-        'Mismatch between model and given anchor and class sizes. ' \
-        'Specify matching anchors and classes with --anchors_path and ' \
-        '--classes_path flags.'
-    print('{} model, anchors, and classes loaded.'.format(model_path))
+        # Generate output tensor targets for filtered bounding boxes.
+        # TODO: Wrap these backend operations with Keras layers.
+        yolo_outputs = yolo_head(self.yolo_model.output, anchors, len(self.class_names))
+        self.input_image_shape = K.placeholder(shape=(2, ))
+        self.boxes, self.scores, self.classes = yolo_eval(
+            yolo_outputs,
+            self.input_image_shape,
+            score_threshold=score_threshold,
+            iou_threshold=iou_threshold)
 
-    # Check if model is fully convolutional, assuming channel last order.
-    model_image_size = yolo_model.layers[0].input_shape[1:3]
-    is_fixed_size = model_image_size != (None, None)
+        self.session = K.get_session()  # TODO: Remove dependence on Tensorflow session.
 
-    # Generate colors for drawing bounding boxes.
+    def detect(self, image_filepath):
+        image = Image.open(image_filepath)
+        image_data = self._transform_image(image)
+
+        out_boxes, out_scores, out_classes = self.session.run(
+            [self.boxes, self.scores, self.classes],
+            feed_dict={
+                self.yolo_model.input: image_data,
+                self.input_image_shape: [image.size[1], image.size[0]],
+                K.learning_phase(): 0
+            })
+
+        objects = []
+
+        for i, c in list(enumerate(out_classes)):
+            predicted_class = self.class_names[c]
+            box = out_boxes[i]
+            score = out_scores[i]
+
+            top, left, bottom, right = box
+            top = max(0, np.floor(top + 0.5).astype('int32'))
+            left = max(0, np.floor(left + 0.5).astype('int32'))
+            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+
+            object = Object(predicted_class, top, left, bottom, right, score)
+            objects.append(object)
+
+        return objects
+
+    def close(self):
+        self.session.close()
+
+    def _load_anchors(self, anchors_path):
+        with open(anchors_path) as f:
+            anchors = f.readline()
+            anchors = [float(x) for x in anchors.split(',')]
+            anchors = np.array(anchors).reshape(-1, 2)
+        return anchors
+
+    def _load_class_names(self, classes_path):
+        with open(classes_path) as f:
+            class_names = f.readlines()
+        return [c.strip() for c in class_names]
+
+    def _transform_image(self, image):
+        if self.is_fixed_size:  # TODO: When resizing we can use minibatch input.
+            resized_image = image.resize(
+                tuple(reversed(self.model_image_size)), Image.BICUBIC)
+            image_data = np.array(resized_image, dtype='float32')
+        else:
+            # Due to skip connection + max pooling in YOLO_v2, inputs must have
+            # width and height as multiples of 32.
+            new_image_size = (image.width - (image.width % 32),
+                              image.height - (image.height % 32))
+            resized_image = image.resize(new_image_size, Image.BICUBIC)
+            image_data = np.array(resized_image, dtype='float32')
+        
+        image_data /= 255.
+        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
+
+        return image_data
+
+
+def _generate_colors(class_names):
     hsv_tuples = [(x / len(class_names), 1., 1.)
                   for x in range(len(class_names))]
     colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
@@ -103,91 +173,78 @@ def _main(args):
     random.seed(10101)  # Fixed seed for consistent colors across runs.
     random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
     random.seed(None)  # Reset seed to default.
+    colors = {class_names[i]: v for i, v in enumerate(colors)}
+    return colors
 
-    # Generate output tensor targets for filtered bounding boxes.
-    # TODO: Wrap these backend operations with Keras layers.
-    yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
-    input_image_shape = K.placeholder(shape=(2, ))
-    boxes, scores, classes = yolo_eval(
-        yolo_outputs,
-        input_image_shape,
-        score_threshold=args.score_threshold,
-        iou_threshold=args.iou_threshold)
 
+def _main(args):
+    model_path = os.path.expanduser(args.model_path)
+    assert model_path.endswith('.h5'), 'Keras model must be a .h5 file.'
+
+    anchors_path = os.path.expanduser(args.anchors_path)
+    classes_path = os.path.expanduser(args.classes_path)
+    test_path = os.path.expanduser(args.test_path)
+    output_path = os.path.expanduser(args.output_path)
+
+    if not os.path.exists(output_path):
+        print('Creating output path {}'.format(output_path))
+        os.mkdir(output_path)
+
+    model = YoloModel(model_path, anchors_path, classes_path, args.score_threshold, args.iou_threshold)
+    print('{} model, anchors, and classes loaded.'.format(model_path))
+
+    # Generate colors for drawing bounding boxes.
+    colors = _generate_colors(model.class_names)
+ 
     for image_file in os.listdir(test_path):
+        image_filepath = os.path.join(test_path, image_file)
         try:
-            image_type = imghdr.what(os.path.join(test_path, image_file))
+            image_type = imghdr.what(image_filepath)
             if not image_type:
                 continue
         except IsADirectoryError:
             continue
 
-        image = Image.open(os.path.join(test_path, image_file))
-        if is_fixed_size:  # TODO: When resizing we can use minibatch input.
-            resized_image = image.resize(
-                tuple(reversed(model_image_size)), Image.BICUBIC)
-            image_data = np.array(resized_image, dtype='float32')
-        else:
-            # Due to skip connection + max pooling in YOLO_v2, inputs must have
-            # width and height as multiples of 32.
-            new_image_size = (image.width - (image.width % 32),
-                              image.height - (image.height % 32))
-            resized_image = image.resize(new_image_size, Image.BICUBIC)
-            image_data = np.array(resized_image, dtype='float32')
-            print(image_data.shape)
+        objects = model.detect(image_filepath)
+        print('Found {} objects for {}'.format(len(objects), image_file))
 
-        image_data /= 255.
-        image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
-
-        out_boxes, out_scores, out_classes = sess.run(
-            [boxes, scores, classes],
-            feed_dict={
-                yolo_model.input: image_data,
-                input_image_shape: [image.size[1], image.size[0]],
-                K.learning_phase(): 0
-            })
-        print('Found {} boxes for {}'.format(len(out_boxes), image_file))
+        image = Image.open(image_filepath)
 
         font = ImageFont.truetype(
             font='font/FiraMono-Medium.otf',
             size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = (image.size[0] + image.size[1]) // 300
 
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
-
-            label = '{} {:.2f}'.format(predicted_class, score)
+        for i, object in reversed(list(enumerate(objects))):
+            label = '{} {:.2f}'.format(object.class_name, object.score)
 
             draw = ImageDraw.Draw(image)
             label_size = draw.textsize(label, font)
 
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-            print(label, (left, top), (right, bottom))
+            print(label, (object.left, object.top), (object.right, object.bottom))
 
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
+            if object.top - label_size[1] >= 0:
+                text_origin = np.array([object.left, object.top - label_size[1]])
             else:
-                text_origin = np.array([left, top + 1])
+                text_origin = np.array([object.left, object.top + 1])
+
+            color = colors[object.class_name]
 
             # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
+            for j in range(thickness):
                 draw.rectangle(
-                    [left + i, top + i, right - i, bottom - i],
-                    outline=colors[c])
+                    [object.left + j, object.top + j, object.right - j, object.bottom - j],
+                    outline=color)
+
             draw.rectangle(
                 [tuple(text_origin), tuple(text_origin + label_size)],
-                fill=colors[c])
+                fill=color)
             draw.text(text_origin, label, fill=(0, 0, 0), font=font)
             del draw
 
         image.save(os.path.join(output_path, image_file), quality=90)
-    sess.close()
+
+    model.close()
 
 
 if __name__ == '__main__':
