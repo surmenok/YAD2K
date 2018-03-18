94b2377eccfc4e157d021e85c51751a3a9578b43
Pavel Surmenok <surmenok@gmail.com>
2018-03-17 17:49:13 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "yad2k",
+    version = 0.1 ,
+    description = "Yet Another Darknet 2 Keras",
+    author = "Allan Zelener and contributors",
+    license = "MIT",
+    url = "https://github.com/allanzelener/YAD2K",
+    install_requires = ['keras', 'h5py', 'pillow'],
+)
